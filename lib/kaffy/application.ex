7c0317cbb1f20efcfe88aa39796c9c761f453f97
saul <zaricmore@gmail.com>
2021-08-02 19:06:10 +0800

    wip

--- lib/kaffy/application.ex
+++ lib/kaffy/application.ex
@@ -6,7 +6,6 @@ defmodule Kaffy2.Application do
   @spec start(any, any) :: {:error, any} | {:ok, pid}
   def start(_type, _args) do
     children = [
-      {Kaffy.Scheduler.Supervisor, []},
       Kaffy.Cache.Client
     ]
 
